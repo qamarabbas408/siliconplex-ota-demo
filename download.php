3be9a3f7c020126ccae8724ca9db96f249f7af04
Qamar Abbas <32230195+qamarabbas408@users.noreply.github.com>
2023-11-24 16:32:48 +0500

    Update download.php

--- download.php
+++ download.php
@@ -5,7 +5,7 @@ header("Content-Disposition: attachment; filename='ead.ipa'");
 // Replace 'your_ipa_file.ipa' with your actual .ipa file name
 
 // Path to your .ipa file
-$ipa_file = 'resources/ead.ipa'; 
+$ipa_file = 'https://drive.google.com/file/d/1m77ysx3RopLQJHKZb_sCwkm3rRyDee9D/view?usp=drive_link'; 
 
 // Check if the file exists
 if (file_exists($ipa_file)) {
